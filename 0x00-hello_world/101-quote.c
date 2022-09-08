32756320f83ec96b4286c5c2e3b04136ac19ea44
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-08 13:32:21 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
- * Return: Always 1(Success)
+ * main Prints a text
+ *
+ * Return: Always 1 (Success)
  */
-int main(void)
 {
-	write(STDOUT_FILENO, "and that pice of art is useful\" -Dora Korpar, 2015-10-19\n". 59);
-	return (1)
+	write(2,"and that piece od art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
