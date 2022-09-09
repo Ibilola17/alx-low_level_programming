0867c949260b9d06f325c79e5f21de98924e1c69
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-09 02:50:01 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Seccess/correct)
+ */
+int main(void)
+{
+	chat alphabet;
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0)
+}
