b71d247883fdf628eec90a63fee01487a8023eb4
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-09 02:58:20 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ */
+int main(void)
+
+{
+	char lower_case;
+	char upper_case;
+
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+	{
+		putchar(lower_case);
+	}
+
+	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+	{
+		putchar(upper_case);
+	}
+
+
+	putchar('\n');
+	return (0);
+}
+
