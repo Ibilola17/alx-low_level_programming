3e2b00c1de43c7a904bdf152cbb5d1e3300c431f
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-09 14:28:33 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,15 +11,15 @@ int main(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		putchar(c);
+		putchar (c);
 	}
 
 	for (c = 'a'; c <= 'f'; c++)
 	{
-		putchar(c);
+		putchar (c);
 	}
 
-	putchar('\n');
+	putchar ('\n');
 
 	return(0);
 }
