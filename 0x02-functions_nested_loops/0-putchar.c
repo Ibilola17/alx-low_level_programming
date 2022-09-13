bd7adef8922be00f7c20fc7fab2cf3cd7642ac50
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-13 04:25:57 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include "main.h"
 
 /** main - prints putchar
  * Return: always 0
  */
 int main(void)
 {
-	_putchar("_");
-	_putchat("p");
+	_putchar(" ");
+	_putchar("p");
 	_putchar("u");
 	_putchar("t");
 	_putchar("c");
