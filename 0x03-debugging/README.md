fff3cf1993fd91816bf6874dba57305a354ca713
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-14 04:07:32 -0700

    0-main.c

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,5 @@
+0x03. C -Debugging
+By Carrie Ybay
+ongoing project - started 08-31-2021, must end by 09-03-2021(in 1 day) - you're done with 0% of tasks.
+checker was released at 09-01-2021 12:00pm
+QA review fully automated
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int, int, int);
+void printy_remaining_days(int, int, int);
+int convert_day(int, int);
+
+#endif /* MAIN_H */
