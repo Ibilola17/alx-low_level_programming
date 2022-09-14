6037135375a0187453922ad6d15c111cf54b7842
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-13 21:30:36 -0700

    Task 8

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,18 @@
 #include "main.h"
 
 /**
- * print_last_digit - prints the last digit of a number.
- * @n: The number in question
+ * print_last_digit - Prints the last digit of a number.
+ * @n: The number in question.
  *
- * Return:value of the last digit
+ * Return: Value of the last digit.
  */
-int print_last_digit(int n);
+
+int print_last_digit(int n)
 {
 	int last_digit = n % 10;
+
+	if (last_digit < 0)
+		last_digit *= -1;
 
 	if (last_digit < 0)
 		last_digit *= -1;
@@ -17,4 +21,3 @@ int print_last_digit(int n);
 
 	return (last_digit);
 }
-
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * jack_bauer - Prints every minute of the day of
  * Jack Bauer, starting from 00:00 to 23:59.
@@ -7,16 +8,17 @@
 void jack_bauer(void)
 {
 	int hour, minute;
-	for (hour = 0; hour <= 23; hour++)
-	{
-		for (minute = 0; minute <= 59; minute++)
+
+		for (hour = 0; hour <= 23; hour++)
 		{
-			_putchar((hour / 10) + '0');
-			_putchar((hour % 10) + '0');
-			_putchar(':');
-			_putchar((minute / 10) + '0');
-			_putchar((minute % 10) + '0');
-			_putchar('\n');
+			for (minute = 0; minute <= 59; minute++)
+			{
+				_putchar((hour / 10) + '0');
+				_putchar((hour % 10) + '0');
+				_putchar(':');
+				_putchar((minute / 10) + '0');
+				_putchar((minute % 10) + '0');
+				_putchar('\n');
+			}
 		}
-	}
 }
