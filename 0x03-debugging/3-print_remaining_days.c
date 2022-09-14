cbb60279cf6f57bbab0fb261dbc8acdbea78b844
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-14 12:27:38 -0700

    tsk 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,7 +11,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 400 == 0) || (year % 4 == 0 && year 100 != 0))
+	if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
 	{
 		if (month >= 3 && day >= 60)
 		{
@@ -30,7 +30,7 @@ void print_remaining_days(int month, int day, int year)
 		else
 		{
 			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
+			printf("Remaining days: &d\n", 365 - day);
 		}
 	}
 }
