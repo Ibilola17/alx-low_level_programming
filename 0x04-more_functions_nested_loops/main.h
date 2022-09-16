301f67375c9faeca489e5477444fcda8cecc5167
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-16 02:35:41 -0700

    task 10

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -15,7 +15,7 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-int _putchar(char c);
+int _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
