513517ad86ba1850ef5524f0a9a193a6988a55f3
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-15 19:23:22 -0700

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,7 +13,7 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-		for (inc = 1; inc1 <= size; inc1++)
+		for (inc1 = 1; inc1 <= size; inc1++)
 		{
 			for ((inc2 = size - inc1); inc2 > 0; inc2--)
 			{
