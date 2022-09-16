2ed964b64ede4c8266e04735798e5a79f2e55f3f
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-15 21:28:45 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the numbers from 1 to 100, But for multiples of three
- * print Fizz instead of the number and for the multiples of five print Buzz
- * For numbers which are multiples of both three and five print FizzBuzz
+ * main - prints the numbers from 1 to 100
  * Return: Always 0 (success)
  */
 
