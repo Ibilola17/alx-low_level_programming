1c89508b24e433cedb55c0de5d0facc8f9f4f97c
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-15 21:15:05 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,7 +16,7 @@ int main(void)
 	{
 		if (i == 100)
 			printf("Buzz");
-		else if(((i % 3) == 0) && ((i % 5) == 0))
+		else if (((i % 3) == 0) && ((i % 5) == 0))
 			printf("FizzBuzz");
 		else if ((i % 3) == 0)
 			printf("Fizz");
