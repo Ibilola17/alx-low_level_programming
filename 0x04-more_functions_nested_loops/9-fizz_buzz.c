e5d14a3850f01d47e65cbc8cf34d552b3145c85b
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-15 21:49:56 -0700

    Hot fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,13 +16,13 @@ int main(void)
 		if (i == 100)
 			printf("Buzz ");
 		else if (((i % 3) == 0) && ((i % 5) == 0))
-			printf("FizzBuz z");
+			printf("FizzBuzz");
 		else if ((i % 3) == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if ((i % 5) == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else
-			printf("%d ", i);
+			printf("%d", i);
 	}
 	printf("\n");
 	return (0);
