8000ae8a35d412c8b9c77b287217b9f974f09bdb
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-19 07:31:53 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -23,10 +23,13 @@ int _atoi(char *s)
 	}
 	if (*temp != '\0')
 	{
-		do{
+		do
+		{
 			num = num * 10 + (*temp - '0');
 			temp++;
-		} while (*temp >= '0' && *temp <= '9');
+		}
+		while (*temp >= '0' && *temp <= '9');
 	}
-	return (num * sign);
+	return (num *sign);
 }
+
