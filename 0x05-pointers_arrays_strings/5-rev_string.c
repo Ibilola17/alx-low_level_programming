1592d56095f756a67fdb16223ca8d26b379181ea
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-19 03:53:09 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * rev_string- prints in reverse
+ * _strlen- count
+ * @s: checked
+ * Return: nothing
+ */
+
+int _strlen(char *s)
+{
+
+	int i, len;
+
+	len = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len++;
+	}
+	return (len);
+}
+
+void rev_string(char *s)
+{
+	int strsize = _strlen(s);
+	int i;
+
+	char c;
+
+	i = 0;
+
+	while (i < (strsize / 2))
+	{
+		c = *(s + i);
+		*(s + i) = *(s + strsize - 1 - i);
+		*(s + strsize - 1 - i) = c;
+		i++;
+	}
+}
+
