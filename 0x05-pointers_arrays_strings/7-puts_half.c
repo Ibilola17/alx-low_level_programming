46d20b68946059b4925e84250103c5eb763238ef
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-19 07:26:17 -0700

    task 10

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts_half - function that printshalf of a string, followed by a new line
+ * puts_half - function that prints half of a string, followed by a new line
  *
  * @str: This is the input string
  */
