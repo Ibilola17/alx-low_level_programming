d7f51573331369f0ce2012061ae9504735cd5958
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-19 03:22:08 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,14 +10,13 @@
 
 void puts2(char *str)
 {
-	int 1 = 0;
+	int i = 0;
 
 	while (*(str + i) != '\0')
 	{
-		if (i % 2 ==0)
+		if (i % 2 == 0)
 			putchar(*(str + i));
 		i++;
 	}
 	putchar(10);
 }
-
