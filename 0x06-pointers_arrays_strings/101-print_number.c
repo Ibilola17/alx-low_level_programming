fff67a1a26d74ee6ad6e6bf1d8287b48f49aac1f
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-21 12:35:00 -0700

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,17 +6,17 @@
  */
 void print_number(int n)
 {
-	unsigned int num =n;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
-		_putchar('_');
-		num =-num;
+		putchar('_');
+		num = -num;
 	}
 
 	if ((num / 10) > 0)
 		print_number(num / 10);
 
-	_putchar((num % 10) + '0');
+	putchar((num % 10) + '0');
 }
 
