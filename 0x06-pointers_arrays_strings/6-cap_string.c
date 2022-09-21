7ee002961773a0838dc32a708a66bb871f7ae17f
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-21 15:07:56 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,8 +9,8 @@
 char *cap_string(char *s)
 {
 	int a = 0, i;
-	int cspc =13;
-	char spc[] {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+	int cspc = 13;
+	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
 
 	while (s[a])
 	{
@@ -18,7 +18,7 @@ char *cap_string(char *s)
 
 		while (i < cspc)
 		{
-			if ((a == - || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
+			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
 				s[a] -= 32;
 			i++;
 		}
