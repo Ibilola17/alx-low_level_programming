dbdcee4613e8b6b75b857f5eb13b949581728646
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-21 10:58:36 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,11 +9,7 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
-	dest_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
+	strncat(dest, src, n);
 
 	return (dest);
 }
