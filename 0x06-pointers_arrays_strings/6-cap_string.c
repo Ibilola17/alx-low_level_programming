add10e336ea5d104c8a65495f7fa4dff1a7ac8b8
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-21 11:28:28 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,52 +1,28 @@
 #include "main.h"
 
 /**
- * _strlen - returns the lenght of a string
- * @s: string
- * Return: returns lenght as integer;
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*(s + len) != '\0')
-		len++;
-
-	return (len);
-}
-
-/**
- * cap_string - function that capitalize first chracter of a word
- * @str: string to capitalize
- * Return: returns the capitalized string
+ * cap_string - Function that capitalizes each word in a string
+ * @str: The input string
+ * Return: Always the capitalized string
  */
 
 char *cap_string(char *str)
 {
-	int index = 0;
+	char sprts[13] = " \t\n,;.!?\"(){}";
+	int len,
+	    i,
+	    j;
 
-	while (str[++index])
+	len = strlen(str);
+	for (i = 0; i < len; i++)
 	{
-		while (!(str[index] >= '0') && (str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-				str[index - 1] == '\t' ||
-				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||
-				str[index - 1] == ';' ||
-				str[index - 1] == '.' ||
-				str[index - 1] == '!' ||
-				str[index - 1] == '?' ||
-				str[index - 1] == '"' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')' ||
-				str[index - 1] == '{' ||
-				str[index - 1] == '}' ||
-				str[index - 1] == '}')
-			str[index] -= 32;
+		for (j = 0; j < 13; j++)
+		{
+			if (str[i] == sprts[j])
+				str[i + 1] = toupper(str[i + 1]);
+		}
 	}
+
 	return (str);
 }
 
