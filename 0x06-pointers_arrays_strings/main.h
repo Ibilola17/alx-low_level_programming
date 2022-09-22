3a7423ac507048e879b6157eb1f08568650a18ba
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-22 03:44:05 -0700

    task 8

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+/**
+ * void prototypes()
+ * int prototypes()
+ * char prototypes()
+ */
 
 void reverse_array(int *a, int n);
 void print_number(int n);
@@ -17,4 +22,4 @@ char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
 
-#enif
+#endif/*MAIN_H*/
