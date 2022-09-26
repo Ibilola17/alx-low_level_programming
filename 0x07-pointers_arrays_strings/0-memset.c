3cf7385573269895dee1b77bbbaaca52ebcfd3c5
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-26 02:15:30 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -14,10 +14,10 @@
 void *_memset(void *s, int c, size_t n)
 {
 unsigned int index;
-unsigned char *memory - s, value - c;
+unsigned char *memory = s, value = c;
 
 for (index = 0; index < n; index++)
-memory[index] - value;
+memory[index] = value;
 
 return (memory);
 }
