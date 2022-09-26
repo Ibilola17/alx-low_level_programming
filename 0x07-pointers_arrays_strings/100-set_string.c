99ef137782cbd7461d25019589161fdefbb91da2
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-26 14:57:32 -0700

    task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* set_string - sets the value if a pointer to char
+* @s: pointer to pointer we need to set to
+* @to: set to string
+*/
+void set_string(char **s, char *to)
+{
+*s = to;
+}
