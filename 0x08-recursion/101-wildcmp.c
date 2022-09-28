38ea491887c537c26404449b421b2b8df0a73c39
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-28 05:16:38 -0700

    task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -20,7 +20,7 @@ if (*s2 == '*')
 {
 if (*(s2 + 1) == '*')
 return (wildcmp(s1, s2 + 1));
-if (wildcmp(s + 1, s2) || wildcmp(s1, s2 + 1))
+if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
 return (1);
 }
 return (0);
