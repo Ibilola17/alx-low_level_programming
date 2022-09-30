39dd77fda0a7c3db0f69c271c12bc787a13c6173
Ibilola17 <Adekolazainab204@gmail.com>
2022-09-30 09:32:05 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - this program prints its name
+ * @argc:number of argunments
+ * @argv:strings of arguments
+ *
+ * Return: 0
+ */
+int main(int argc _attribute_((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+a file containg 0x0A-argc_argv
