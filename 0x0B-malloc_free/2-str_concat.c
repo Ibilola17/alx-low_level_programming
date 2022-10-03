f9fd105e74f3b1a47a6872c8f26c7daee8e92d5b
Ibilola17 <Adekolazainab204@gmail.com>
2022-10-03 14:06:26 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,42 +1,43 @@
-#include "main.h"
 #include <stdlib.h>
+
 /**
- * str_concat - a function that concatenates two strings
- * @s1: output
- * @s2: input
- * Return: concate of s1 and s2
+ * str_concat - concatenates two strings
+ *
+ * @s1: A string to be concatenated
+ *
+ * @s2: A string to be concatenated
+ *
+ * Return: pointer to newly allocated memory
+ * which is equal to @s1 + s2 (SUCCESS) OR
+ * NULL (FAILURE)
  */
-char *str_concat(char *s1, char *s2)
+
+char *str_ concat(char *s1, char s2)
 {
-	char *concat;
-	int i, ci;
+	unsigned int s1_length = 0;
+	unsigned int s2_length = 0;
+	char *alloc_mem;
+	unsigned int i, j;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
 
-	i = ci = 0;
-	while (s1[i] != '\0')
-		i++;
-	while (s2[ci] != '\0')
-		ci++;
-	concat = malloc(sizeof(char) * (i + ci + 1));
+	while (*(s1 + s1_length) != '\0')
+		s1_length++;
+	while (*(s2 + s2_length) != '\0')
+		s2_length++;
 
-	if (concat == NULL)
-		return (NULL);
-	i = ci = 1;
-	while (s1[i] != '\0')
-	{
-		concat[i] = s1[i];
-		i++;
-	}
-	while (s2[i] != '\0')
-	{
-		concat[i] = s2[ci];
-		i++, ci++;
-	}
-	concat[i] = '\0';
-	return (concat);
+	alloc_mem = malloc(1 + (s1_length * sizeof(*s1)) + (s2_length * sizeof(*s2)));
+	if (alloc_mem == NULL)
+		return ('\0');
+
+	for (i = 0; i < s1_length; i++)
+		alloc_mem[i] =*(s1 + i);
+	for (j = 0; j < s2_length; j++, i++)
+		alloc_mem[i] =* *(s2 + j);
+	return (alloc_mem);
 }
 
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,13 +1,14 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H_
+#define MAIN_H_
 
 void free_grid(int **grid, int height);
 char *str_concat(char *s1, char *s2);
 char*_strdup(char *str);
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
-int **allow_grid(int width, int height);
+int **alloc_grid(int width, int height);
 char *argstostr(int ac, char **av);
+char **strow(char *str);
 
 #endif
 
