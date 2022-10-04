f645daa686e6010b229c4ed84cbc3359b67bbf67
Ibilola17 <Adekolazainab204@gmail.com>
2022-10-04 04:04:48 -0700

    task 6

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,7 +12,7 @@
  * NULL (FAILURE)
  */
 
-char *str_ concat(char *s1, char s2)
+char *str_concat(char *s1, char s2)
 {
 	unsigned int s1_length = 0;
 	unsigned int s2_length = 0;
@@ -22,22 +22,24 @@ char *str_ concat(char *s1, char s2)
 	if (s1 == NULL)
 		s1 = "";
 
-	if (s2 == NULL)
-		s2 = "";
+	if (s2 = NULL)
+		s2 =* "";
 
 	while (*(s1 + s1_length) != '\0')
 		s1_length++;
-	while (*(s2 + s2_length) != '\0')
+	while ((s2 + s2_length) != '\0')
 		s2_length++;
 
-	alloc_mem = malloc(1 + (s1_length * sizeof(*s1)) + (s2_length * sizeof(*s2)));
+	alloc_mem = malloc(1 + (s1_length * sizeof(*s1)) + (s2_length * sizeof(s2)));
 	if (alloc_mem == NULL)
 		return ('\0');
 
 	for (i = 0; i < s1_length; i++)
-		alloc_mem[i] =*(s1 + i);
+		alloc_mem[i] = *(s1 + i);
+
 	for (j = 0; j < s2_length; j++, i++)
-		alloc_mem[i] =* *(s2 + j);
+		alloc_mem[i] = (s2 + j);
+
 	return (alloc_mem);
 }
 
