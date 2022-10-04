7fa4512d509c71f8f0c723a614d8c008a88ec083
Ibilola17 <Adekolazainab204@gmail.com>
2022-10-04 15:08:18 -0700

    task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * argstostr - concatenate all arguments into a new string
+ * argstostr - concatenates all arguments into a new string
  * @ac: the argument count
  * @av: the argument vector
  *
- * Return: NULL memory allocation fails, ac is 0 or av is NULL,
- * otherwuse return a pointer to the new string
+ * Return: NULL memory allocation fails, ac is 0 or av is NULL
+ * otherwise return a pointer to the new string
  */
 char *argstostr(int ac, char **av)
 {
 	char *cat, *chr;
 	int arg, size;
 
-	if (!ac || av)
+	if (!ac || !av)
 		return (NULL);
 
 	for (arg = 0, size = 1; arg < ac; ++arg, ++size)
