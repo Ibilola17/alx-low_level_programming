e27262798a5d8f1264bb6e5e734a0dfb00bfaff0
Ibilola17 <Adekolazainab204@gmail.com>
2022-10-12 06:43:15 -0700

    task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * main - prints its own upcode
  * @argc: number of arguments
  * @argv: argument vector
  *
- * Return: 0
+ * Return: Always 0(success)
  */
 int main(int argc, char *argv[])
 {
-	int count, bytes;
+	int bytes, i;
+	char *arr;
 
 	if (argc != 2)
 	{
@@ -18,19 +20,23 @@ int main(int argc, char *argv[])
 	}
 
 	bytes = atoi(argv[1]);
+
 	if (bytes < 0)
 	{
 		printf("Error/n");
 		exit(2);
 	}
 
-	for (count = 0; count < bytes; count++)
+	arr = (char *)main;
+
+	for (i = 0; i < bytes; i++)
 	{
-		printf("%02hhx", *((char *)main + count));
-		if (count < bytes - 1)
-			printf(" ");
-		else
-			printf("\n");
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
 	}
 	return (0);
 }
