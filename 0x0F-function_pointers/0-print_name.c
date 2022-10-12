c1be58ebb8297e12d7a14e969703ff55a4e561a6
Ibilola17 <Adekolazainab204@gmail.com>
2022-10-12 04:33:26 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: The name to print
+ * @f: pointer to function
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
+
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+a file containing 0x0F-function_pointers
